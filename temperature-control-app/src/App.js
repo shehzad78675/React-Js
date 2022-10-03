13190d5420238666fbf68736ac7a721dcfac4cee
Shehzad Muhammad <76575912+shehzad78675@users.noreply.github.com>
2022-10-03 15:05:05 +0500

    Add temperature control into temperature control branch

---
+++ temperature-control-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TemperatureControl from './components/TemperatureControl';
+
+function App() {
+  return (
+    <div className="App">
+      <TemperatureControl/>
+    </div>
+  );
+}
+
+export default App;
